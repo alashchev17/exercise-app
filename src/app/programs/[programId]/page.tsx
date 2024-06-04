3c50c2742e3fe1160b84d6bf2892a3185ec7d6b1
alashchev17 <andrew.lashchev15@gmail.com>
2024-06-04 22:58:58 +0200

    fix: route programId handling to properly render a page

--- src/app/programs/[programId]/page.tsx
+++ src/app/programs/[programId]/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { sdk } from "@/lib/client";
 import { IProgram } from "@/lib/models/IProgram";
 import { IWorkout } from "@/lib/models/IWorkout";
+import Link from "next/link";
 
 interface ProgramPageProps {
   params: {
@@ -9,23 +10,43 @@ interface ProgramPageProps {
   };
 }
 
-async function getProgramById(programId: string): Promise<IProgram> {
+async function getProgramById(
+  programId: string,
+): Promise<IProgram | undefined | null> {
   const response = await sdk.getProgram({ programId });
-  return response.data.program as IProgram;
+  return response.data.program;
 }
 
 export async function generateMetadata({
   params,
 }: ProgramPageProps): Promise<Metadata> {
-  const program: IProgram = await getProgramById(params.programId);
+  const program = await getProgramById(params.programId);
+  if (!program) {
+    return {
+      title: "Program not found",
+    };
+  }
+
   return {
     title: `Program | ${program.title}`,
   };
 }
 
 export default async function ProgramPage({ params }: ProgramPageProps) {
-  const program: IProgram = await getProgramById(params.programId);
-  const workouts: IWorkout[] = program?.workouts;
+  const program: IProgram | undefined | null = await getProgramById(
+    params.programId,
+  );
+  const workouts: IWorkout[] | undefined = program?.workouts;
+
+  if (!program) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Program which you are looking for does not exist
+        <Link href={"/"}>Go back</Link>
+      </main>
+    );
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Program Page {program.title}
