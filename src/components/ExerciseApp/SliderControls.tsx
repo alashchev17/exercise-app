0eb99c4c6bfb967ec64fd81f3179976fe41fd0f5
alashchev17 <andrew.lashchev15@gmail.com>
2024-06-07 14:36:54 +0200

    fix: SliderControls fix

--- src/components/ExerciseApp/SliderControls.tsx
+++ src/components/ExerciseApp/SliderControls.tsx
@@ -7,9 +7,9 @@ interface SliderControlsProps {
 
 export const SliderControls = ({ next, prev, prevDisabled, nextDisabled }: SliderControlsProps) => {
   return (
-    <div className="text-center absolute" style={{ textAlign: 'center' }}>
+    <div className="w-full left-0 bottom-4 text-center fixed bg-white z-10" style={{ textAlign: 'center' }}>
       <button
-        className="fixed left-4 bottom-4 w-[calc(50%-1.5rem)] px-6 py-4 bg-zinc-800 text-white font-semibold shadow-lg z-10 aria-disabled:bg-gray-300 aria-disabled:text-gray-800 transition-all"
+        className=" w-[calc(50%-1.5rem)] mr-4 px-6 py-4 bg-zinc-800 text-white font-semibold shadow-lg aria-disabled:bg-gray-300 aria-disabled:text-gray-800 transition-all"
         onClick={prev}
         disabled={prevDisabled}
         aria-disabled={prevDisabled}
@@ -17,7 +17,7 @@ export const SliderControls = ({ next, prev, prevDisabled, nextDisabled }: Slide
         Previous
       </button>
       <button
-        className="fixed right-4 bottom-4 w-[calc(50%-1.5rem)] px-6 py-4 bg-zinc-800 text-white font-semibold shadow-lg z-10 aria-disabled:bg-gray-300 aria-disabled:text-gray-800 transition-all"
+        className="w-[calc(50%-1.5rem)] px-6 py-4 bg-zinc-800 text-white font-semibold shadow-lg aria-disabled:bg-gray-300 aria-disabled:text-gray-800 transition-all"
         onClick={next}
         disabled={nextDisabled}
         aria-disabled={nextDisabled}
