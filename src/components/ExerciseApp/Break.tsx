caf6e43041b41305bcd086d977c169549e976773
alashchev17 <andrew.lashchev15@gmail.com>
2024-06-07 09:43:50 +0200

    feat: production-candidate commit

--- src/components/ExerciseApp/Break.tsx
+++ src/components/ExerciseApp/Break.tsx
@@ -1,5 +1,6 @@
 import { Heading } from '@/components/UI/Heading'
 import { Timer } from '@/components/UI/Timer'
+import { Video } from '@/components/UI/Video'
 import type { ExerciseWithBreak } from '@/lib/utils'
 
 interface ExerciseProps {
@@ -15,9 +16,10 @@ export const Break = ({ exercise, countForTimer, onFinished }: ExerciseProps) =>
       <Heading level={2} className="w-full text-center pb-4 text-zinc-500 border-b">
         {exerciseToRender.title}
       </Heading>
-      <Heading level={3} className="w-full text-center pb-4">
+      <Heading level={3} className="w-full text-center">
         {"It's time to break!"}
       </Heading>
+      <Video videoUrl={exerciseToRender.videoUrl!} />
       <Timer countStart={countForTimer} onFinished={onFinished} />
       <Heading level={3} className="w-full text-center pb-4">
         Current set: {exerciseToRender.currentSet}
